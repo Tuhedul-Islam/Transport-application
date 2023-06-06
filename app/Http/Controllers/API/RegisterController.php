d8608ce89f2c626886b4cc820bf814833195c5a5
Touhid <aoradev04@gmail.com>
2023-06-06 08:10:58 +0600

    Transport application

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()){
+            return BaseController::sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['user_info'] =  $user;
+        return BaseController::sendResponse($success, 'User register successfully.');
+
+    }
+
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')-> accessToken;
+            $success['user_info'] =  $user;
+            return BaseController::sendResponse($success, 'User login successfully.');
+        }
+        else{
+            return BaseController::sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+    }
+
+}
---
+++ app/Models/Transport.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transport_way', 'transport_name'
+    ];
+}
